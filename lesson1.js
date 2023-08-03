6eaaf4dbf0b3e9ace5b38d1abaecef16beecce65
Premsagar Gedam <gedamprems@gmail.com>
2023-08-04 03:19:29 +0530

    added lesson 1

---
+++ lesson1.js
@@ -0,0 +1,20 @@
+// http://api.weatherstack.com/current?access_key=1a4a80bacc0ba275f37c21d265194eb4&query=37.8267,-122.4233
+
+const request = require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=1a4a80bacc0ba275f37c21d265194eb4&query=37.8267,-122.4233'
+
+request({ url: url, json: true}, (error, response) => {
+    // const data = JSON.parse(response.body)
+    // console.log(data.current.weather_descriptions[0]+". It is currently "+data.current.temperature+" out. It feels like "+data.current.feelslike+" degress out.")
+    
+    // added json: true in the object { url: url, json: true}
+    const temp = response.body.current.temperature;
+    const feelslike = response.body.current.feelslike
+    const weatherCondition = response.body.current.weather_descriptions[0]
+
+    console.log(weatherCondition+". It is currently "+temp+" out. It feelslike "+feelslike+" degree out.")
+
+})
+
+
