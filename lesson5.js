52aab31c2cddc18f21692f7ad476b98c4ffc3034
Premsagar Gedam <gedamprems@gmail.com>
2023-08-04 06:18:53 +0530

    done callback chaining and also done taking argument from terminal and getting the result

--- lesson5.js
+++ lesson5.js
@@ -3,11 +3,23 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('new york',(error,data)=>{
-    console.log(error, data)
-})
+// added command link argument for address
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+const address = process.argv[2]
+if(!address){
+    console.log("Please provide an address!")
+}else{
+    geocode(address,(error,data)=>{
+        if(error){
+            return console.log(error)
+        }
+
+        forecast(data.longitude, data.latitude, (error, forecastData) => {
+            if(error){
+                return console.log(error)
+            }
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
